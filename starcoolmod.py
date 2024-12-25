407c42e8550d392737121ac2bb26f662a9d9f43f
Sandi20030425 <botalltier5v@gmail.com>
2024-12-25 21:50:07 +0700

    Update starcoolmod.py

--- starcoolmod.py
+++ starcoolmod.py
@@ -2,7 +2,7 @@ import requests
 from time import sleep
 BASE_URL: str = "https://www.cpmewan1999.cpmewan.com/api"
 
-class StarcoolMod:
+class StarcoolMod1:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
